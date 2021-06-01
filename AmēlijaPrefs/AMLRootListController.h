1b27e8ce8fd52a3492719ca3dc4f31ae01c96b03
Luki120 <lukicassino@hotmail.com>
2021-06-01 20:29:00 -0300

    Dynamic specifiers + hot changelog view

--- AmēlijaPrefs/AMLRootListController.h
+++ AmēlijaPrefs/AMLRootListController.h
@@ -1,8 +1,47 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 #import <Preferences/PSTableCell.h>
+#import <AudioToolbox/AudioServices.h>
 
 
+
+@interface OBButtonTray : UIView
+@property (nonatomic,retain) UIVisualEffectView * effectView;
+- (void)addButton:(id)arg1;
+- (void)addCaptionText:(id)arg1;;
+@end
+
+
+@interface OBBoldTrayButton : UIButton
+- (void)setTitle:(id)arg1 forState:(unsigned long long)arg2;
++ (id)buttonWithType:(long long)arg1;
+@end
+
+
+@interface OBWelcomeController : UIViewController
+@property (nonatomic, retain) UIView * viewIfLoaded;
+@property (nonatomic, strong) UIColor * backgroundColor;
+@property (assign, nonatomic) BOOL _shouldInlineButtontray;
+- (OBButtonTray *)buttonTray;
+- (id)initWithTitle:(id)arg1 detailText:(id)arg2 icon:(id)arg3;
+- (void)addBulletedListItemWithTitle:(id)arg1 description:(id)arg2 image:(id)arg3;
+@end
+
+
+@interface _UIBackdropViewSettings : NSObject
++ (id)settingsForStyle:(long long)arg1;
+@end
+
+@interface _UIBackdropView : UIView
+- (id)initWithFrame:(CGRect)arg1 autosizesToFitSuperview:(BOOL)arg2 settings:(id)arg3;
+- (id)initWithSettings:(id)arg1;
+@property (assign, nonatomic) BOOL blurRadiusSetOnce;
+@property (assign, nonatomic) double _blurRadius;
+@property (nonatomic, copy) NSString * _blurQuality;
+@end
+
+
+UIBarButtonItem *changelogButtonItem;
 
 
 @interface AmelijaTableCell : PSTableCell
@@ -17,16 +56,31 @@
 @end
 
 
-@interface AMLRootListController : PSListController
-- (void)loadPrefs;
+@interface AMLRootListController : PSListController {
+
+	UITableView * _table;
+
+}
+@property (nonatomic, retain) UIView *headerView;
+@property (nonatomic, retain) UIImageView *headerImageView;
+@property (nonatomic, retain) OBWelcomeController *changelogController;
+- (void)showWtfChangedInThisVersion:(UIButton *)sender;
+//- (void)loadPrefs;
+@end
+
+
+@interface PSListController (Private)
+- (BOOL)containsSpecifier:(PSSpecifier *)arg1;
 @end
 
 
 @interface LSRootListController : PSListController
+@property (nonatomic, retain) NSMutableDictionary *savedSpecifiers;
 @end
 
 
 @interface HSRootListController : PSListController
+@property (nonatomic, retain) NSMutableDictionary *savedSpecifiers;
 @end
 
 
--- AmēlijaPrefs/AMLRootListController.m
+++ AmēlijaPrefs/AMLRootListController.m
@@ -65,10 +65,116 @@ static NSString *takeMeThere = @"/var/mobile/Library/Preferences/me.luki.amēlij
 	if (!_specifiers) {
 
 		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
-
+		
 	}
 
 	return _specifiers;
+
+}
+
+
+- (void)viewDidLoad {
+
+
+	[super viewDidLoad];
+    //[self reloadSpecifiers];
+
+
+ 	UIImage *banner = [UIImage imageWithContentsOfFile:@"/Library/PreferenceBundles/AmēlijaPrefs.bundle/hotbanner.png"];
+
+	self.headerView = [[UIView alloc] initWithFrame:CGRectMake(0,0,UIScreen.mainScreen.bounds.size.width,UIScreen.mainScreen.bounds.size.width * banner.size.height / banner.size.width)];
+	self.headerImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,200,200)];
+	self.headerImageView.contentMode = UIViewContentModeScaleAspectFill;
+	self.headerImageView.image = banner;
+	self.headerImageView.translatesAutoresizingMaskIntoConstraints = NO;
+
+
+	[self.headerView addSubview:self.headerImageView];
+
+
+	UIButton *changelogButton =  [UIButton buttonWithType:UIButtonTypeCustom];
+	changelogButton.frame = CGRectMake(0,0,30,30);
+	changelogButton.layer.cornerRadius = changelogButton.frame.size.height / 2;
+	changelogButton.layer.masksToBounds = YES;
+	[changelogButton setImage:[UIImage systemImageNamed:@"checkmark.circle"] forState:UIControlStateNormal];
+	[changelogButton addTarget:self action:@selector(showWtfChangedInThisVersion:) forControlEvents:UIControlEventTouchUpInside];
+	changelogButton.tintColor = [UIColor colorWithRed: 0.47 green: 0.21 blue: 0.24 alpha: 1.00];
+
+
+	changelogButtonItem = [[UIBarButtonItem alloc] initWithCustomView:changelogButton];
+
+	self.navigationItem.rightBarButtonItem = changelogButtonItem;
+
+
+	[NSLayoutConstraint activateConstraints:@[
+
+        [self.headerImageView.topAnchor constraintEqualToAnchor:self.headerView.topAnchor],
+        [self.headerImageView.leadingAnchor constraintEqualToAnchor:self.headerView.leadingAnchor],
+        [self.headerImageView.trailingAnchor constraintEqualToAnchor:self.headerView.trailingAnchor],   
+        [self.headerImageView.bottomAnchor constraintEqualToAnchor:self.headerView.bottomAnchor],
+
+	]];
+
+	_table.tableHeaderView = self.headerView;
+
+}
+
+
+- (void)showWtfChangedInThisVersion:(id)sender {
+    
+    AudioServicesPlaySystemSound(1521);
+
+    self.changelogController = [[OBWelcomeController alloc] initWithTitle:@"Amēlija" detailText:@"1.0" icon:[UIImage imageWithContentsOfFile:@"/Library/PreferenceBundles/AmēlijaPrefs.bundle/HotIcon.png"]];
+
+    [self.changelogController addBulletedListItemWithTitle:@"Initial Release" description:@"" image:[UIImage systemImageNamed:@"exclamationmark.circle.fill"]];
+  
+    //[self.changelogController addBulletedListItemWithTitle:@"Is" description:@"Fucking Hot" image:[UIImage systemImageNamed:@"exclamationmark.circle.fill"]];
+
+    _UIBackdropViewSettings *settings = [_UIBackdropViewSettings settingsForStyle:2];
+
+    _UIBackdropView *backdropView = [[_UIBackdropView alloc] initWithSettings:settings];
+    backdropView.layer.masksToBounds = YES;
+    backdropView.clipsToBounds = YES;
+    backdropView.frame = self.changelogController.viewIfLoaded.frame;
+    [self.changelogController.viewIfLoaded insertSubview:backdropView atIndex:0];
+    
+    backdropView.translatesAutoresizingMaskIntoConstraints = NO;
+    [backdropView.bottomAnchor constraintEqualToAnchor:self.changelogController.viewIfLoaded.bottomAnchor constant:0].active = YES;
+    [backdropView.leftAnchor constraintEqualToAnchor:self.changelogController.viewIfLoaded.leftAnchor constant:0].active = YES;
+    [backdropView.rightAnchor constraintEqualToAnchor:self.changelogController.viewIfLoaded.rightAnchor constant:0].active = YES;
+    [backdropView.topAnchor constraintEqualToAnchor:self.changelogController.viewIfLoaded.topAnchor constant:0].active = YES;
+
+    self.changelogController.viewIfLoaded.backgroundColor = [UIColor clearColor];
+    self.changelogController.view.tintColor = [UIColor colorWithRed: 0.47 green: 0.21 blue: 0.24 alpha: 1.00];
+    self.changelogController.modalPresentationStyle = UIModalPresentationPageSheet;
+    self.changelogController.modalInPresentation = NO;
+    [self presentViewController:self.changelogController animated:YES completion:nil];
+
+}
+
+
+- (void)dismissVC {
+
+    [self.changelogController dismissViewControllerAnimated:YES completion:nil];
+
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    tableView.tableHeaderView = self.headerView;
+    return [super tableView:tableView cellForRowAtIndexPath:indexPath];
+
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+
+	CGFloat offsetY = scrollView.contentOffset.y;
+
+	if (offsetY > 0) offsetY = 0;
+
+	self.headerImageView.frame = CGRectMake(0, offsetY, self.headerView.frame.size.width, 200 - offsetY);
 
 }
 
@@ -98,6 +204,7 @@ static NSString *takeMeThere = @"/var/mobile/Library/Preferences/me.luki.amēlij
 
 }
 
+
 @end
 
 
@@ -112,9 +219,142 @@ static NSString *takeMeThere = @"/var/mobile/Library/Preferences/me.luki.amēlij
 
 		_specifiers = [self loadSpecifiersFromPlistName:@"LS" target:self];
 
-	}
+        NSArray *chosenIDs = @[@"GroupCell-1", @"SliderCell-1", @"GroupCell-3", @"MiscLSBlursList", @"GroupCell-4", @"SliderCell-2"]; //@"GroupCell-5", @"SliderCell-3", @"GroupCell-6", @"GroupCell-7", @"MiscHSBlursList", @"MiscHSBlursList", @"GroupCell-8", @"SliderCell-4"];
+        self.savedSpecifiers = (self.savedSpecifiers) ?: [[NSMutableDictionary alloc] init];
+        for(PSSpecifier *specifier in _specifiers) {
+            if([chosenIDs containsObject:[specifier propertyForKey:@"id"]]) {
+                [self.savedSpecifiers setObject:specifier forKey:[specifier propertyForKey:@"id"]];
+            }
+        }
+    }
 
 	return _specifiers;
+
+}
+
+
+- (void)reloadSpecifiers { // Dynamic specifiers
+
+    [super reloadSpecifiers];
+
+
+    if (![[self readPreferenceValue:[self specifierForID:@"EpicLSBlurSwitch"]] boolValue]) {
+
+        [self removeSpecifier:self.savedSpecifiers[@"GroupCell-1"] animated:NO];
+        [self removeSpecifier:self.savedSpecifiers[@"SliderCell-1"] animated:NO];
+
+    }
+
+
+    else if (![self containsSpecifier:self.savedSpecifiers[@"GroupCell-1"]]) {
+
+        [self insertSpecifier:self.savedSpecifiers[@"GroupCell-1"] afterSpecifierID:@"EpicLSBlurSwitch" animated:NO];
+        [self insertSpecifier:self.savedSpecifiers[@"SliderCell-1"] afterSpecifierID:@"GroupCell-1" animated:NO];
+
+    }
+
+
+    if (![[self readPreferenceValue:[self specifierForID:@"MiscLSBlursSwitch"]] boolValue]) {
+
+        [self removeSpecifier:self.savedSpecifiers[@"GroupCell-3"] animated:NO];
+        [self removeSpecifier:self.savedSpecifiers[@"MiscLSBlursList"] animated:NO];
+        [self removeSpecifier:self.savedSpecifiers[@"GroupCell-4"] animated:NO];
+        [self removeSpecifier:self.savedSpecifiers[@"SliderCell-2"] animated:NO];
+
+    }
+
+
+    else if (![self containsSpecifier:self.savedSpecifiers[@"GroupCell-3"]]) {
+
+        [self insertSpecifier:self.savedSpecifiers[@"GroupCell-3"] afterSpecifierID:@"MiscLSBlursSwitch" animated:NO];
+        [self insertSpecifier:self.savedSpecifiers[@"MiscLSBlursList"] afterSpecifierID:@"GroupCell-3" animated:NO];
+        [self insertSpecifier:self.savedSpecifiers[@"GroupCell-4"] afterSpecifierID:@"MiscLSBlursList" animated:NO];
+        [self insertSpecifier:self.savedSpecifiers[@"SliderCell-2"] afterSpecifierID:@"GroupCell-4" animated:NO];
+
+    }
+
+}
+
+
+- (void)viewDidLoad {
+
+
+    [super viewDidLoad];
+    [self reloadSpecifiers];
+
+
+}
+
+
+- (id)readPreferenceValue:(PSSpecifier*)specifier {
+
+    NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+    [settings addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:takeMeThere]];
+    return (settings[specifier.properties[@"key"]]) ?: specifier.properties[@"default"];
+
+}
+
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
+
+
+    NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+    [settings addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:takeMeThere]];
+    [settings setObject:value forKey:specifier.properties[@"key"]];
+    [settings writeToFile:takeMeThere atomically:YES];
+
+
+    NSString *key = [specifier propertyForKey:@"key"];
+
+
+    if([key isEqualToString:@"epicLSBlur"]) {
+
+        
+        if (![value boolValue]) {
+
+
+            [self removeSpecifier:self.savedSpecifiers[@"GroupCell-1"] animated:YES];
+            [self removeSpecifier:self.savedSpecifiers[@"SliderCell-1"] animated:YES];
+
+
+        }
+
+
+        else if (![self containsSpecifier:self.savedSpecifiers[@"GroupCell-1"]]) {
+
+            [self insertSpecifier:self.savedSpecifiers[@"GroupCell-1"] afterSpecifierID:@"EpicLSBlurSwitch" animated:YES];
+            [self insertSpecifier:self.savedSpecifiers[@"SliderCell-1"] afterSpecifierID:@"GroupCell-1" animated:YES];
+
+
+        }
+
+
+    }
+
+
+    if([key isEqualToString:@"lsBlur"]) {
+        
+
+        if (![value boolValue]) {
+
+            [self removeSpecifier:self.savedSpecifiers[@"GroupCell-3"] animated:YES];
+            [self removeSpecifier:self.savedSpecifiers[@"MiscLSBlursList"] animated:YES];
+            [self removeSpecifier:self.savedSpecifiers[@"GroupCell-4"] animated:YES];
+            [self removeSpecifier:self.savedSpecifiers[@"SliderCell-2"] animated:YES];
+
+        }
+        
+
+        else if (![self containsSpecifier:self.savedSpecifiers[@"GroupCell-3"]]) {
+
+            [self insertSpecifier:self.savedSpecifiers[@"GroupCell-3"] afterSpecifierID:@"MiscLSBlursSwitch" animated:YES];
+            [self insertSpecifier:self.savedSpecifiers[@"MiscLSBlursList"] afterSpecifierID:@"GroupCell-3" animated:YES];
+            [self insertSpecifier:self.savedSpecifiers[@"GroupCell-4"] afterSpecifierID:@"MiscLSBlursList" animated:YES];
+            [self insertSpecifier:self.savedSpecifiers[@"SliderCell-2"] afterSpecifierID:@"GroupCell-4" animated:YES];
+
+        }
+
+    }
 
 }
 
@@ -131,9 +371,140 @@ static NSString *takeMeThere = @"/var/mobile/Library/Preferences/me.luki.amēlij
 
 		_specifiers = [self loadSpecifiersFromPlistName:@"HS" target:self];
 
+        NSArray *chosenIDs = @[@"GroupCell-5", @"SliderCell-3", @"GroupCell-6", @"GroupCell-7", @"MiscHSBlursList", @"MiscHSBlursList", @"GroupCell-8", @"SliderCell-4"];
+        self.savedSpecifiers = (self.savedSpecifiers) ?: [[NSMutableDictionary alloc] init];
+        for(PSSpecifier *specifier in _specifiers) {
+            if([chosenIDs containsObject:[specifier propertyForKey:@"id"]]) {
+                [self.savedSpecifiers setObject:specifier forKey:[specifier propertyForKey:@"id"]];
+            }
+        }
 	}
 
 	return _specifiers;
+
+}
+
+
+- (void)reloadSpecifiers { // Dynamic specifiers
+
+    [super reloadSpecifiers];
+
+
+    if (![[self readPreferenceValue:[self specifierForID:@"EpicHSBlurSwitch"]] boolValue]) {
+
+        [self removeSpecifier:self.savedSpecifiers[@"GroupCell-5"] animated:NO];
+        [self removeSpecifier:self.savedSpecifiers[@"SliderCell-3"] animated:NO];
+
+    }
+
+
+    else if (![self containsSpecifier:self.savedSpecifiers[@"GroupCell-5"]]) {
+
+        [self insertSpecifier:self.savedSpecifiers[@"GroupCell-5"] afterSpecifierID:@"EpicHSBlurSwitch" animated:NO];
+        [self insertSpecifier:self.savedSpecifiers[@"SliderCell-3"] afterSpecifierID:@"GroupCell-5" animated:NO];
+
+    }
+
+
+    if (![[self readPreferenceValue:[self specifierForID:@"MiscHSBlursSwitch"]] boolValue]) {
+
+        [self removeSpecifier:self.savedSpecifiers[@"GroupCell-7"] animated:NO];
+        [self removeSpecifier:self.savedSpecifiers[@"MiscHSBlursList"] animated:NO];
+        [self removeSpecifier:self.savedSpecifiers[@"GroupCell-8"] animated:NO];
+        [self removeSpecifier:self.savedSpecifiers[@"SliderCell-4"] animated:NO];
+
+    }
+
+
+    else if (![self containsSpecifier:self.savedSpecifiers[@"GroupCell-7"]]) {
+
+        [self insertSpecifier:self.savedSpecifiers[@"GroupCell-7"] afterSpecifierID:@"MiscHSBlursSwitch" animated:NO];
+        [self insertSpecifier:self.savedSpecifiers[@"MiscHSBlursList"] afterSpecifierID:@"GroupCell-7" animated:NO];
+        [self insertSpecifier:self.savedSpecifiers[@"GroupCell-8"] afterSpecifierID:@"MiscHSBlursList" animated:NO];
+        [self insertSpecifier:self.savedSpecifiers[@"SliderCell-4"] afterSpecifierID:@"GroupCell-8" animated:NO];
+
+    }
+
+}
+
+
+- (void)viewDidLoad {
+
+
+    [super viewDidLoad];
+    [self reloadSpecifiers];
+
+
+}
+
+
+- (id)readPreferenceValue:(PSSpecifier*)specifier {
+
+    NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+    [settings addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:takeMeThere]];
+    return (settings[specifier.properties[@"key"]]) ?: specifier.properties[@"default"];
+
+}
+
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
+
+
+    NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+    [settings addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:takeMeThere]];
+    [settings setObject:value forKey:specifier.properties[@"key"]];
+    [settings writeToFile:takeMeThere atomically:YES];
+
+
+    NSString *key = [specifier propertyForKey:@"key"];
+
+
+    if([key isEqualToString:@"epicHSBlur"]) {
+
+        
+        if (![value boolValue]) {
+
+
+            [self removeSpecifier:self.savedSpecifiers[@"GroupCell-5"] animated:YES];
+            [self removeSpecifier:self.savedSpecifiers[@"SliderCell-3"] animated:YES];
+
+        }
+
+
+        else if (![self containsSpecifier:self.savedSpecifiers[@"GroupCell-5"]]) {
+
+            [self insertSpecifier:self.savedSpecifiers[@"GroupCell-5"] afterSpecifierID:@"EpicHSBlurSwitch" animated:YES];
+            [self insertSpecifier:self.savedSpecifiers[@"SliderCell-3"] afterSpecifierID:@"GroupCell-5" animated:YES];
+
+        }
+
+
+    }
+
+
+    if([key isEqualToString:@"hsBlur"]) {
+        
+
+        if (![value boolValue]) {
+
+            [self removeSpecifier:self.savedSpecifiers[@"GroupCell-7"] animated:YES];
+            [self removeSpecifier:self.savedSpecifiers[@"MiscHSBlursList"] animated:YES];
+            [self removeSpecifier:self.savedSpecifiers[@"GroupCell-8"] animated:YES];
+            [self removeSpecifier:self.savedSpecifiers[@"SliderCell-4"] animated:YES];
+
+        }
+        
+
+        else if (![self containsSpecifier:self.savedSpecifiers[@"GroupCell-7"]]) {
+
+            [self insertSpecifier:self.savedSpecifiers[@"GroupCell-7"] afterSpecifierID:@"MiscHSBlursSwitch" animated:YES];
+            [self insertSpecifier:self.savedSpecifiers[@"MiscHSBlursList"] afterSpecifierID:@"GroupCell-7" animated:YES];
+            [self insertSpecifier:self.savedSpecifiers[@"GroupCell-8"] afterSpecifierID:@"MiscHSBlursList" animated:YES];
+            [self insertSpecifier:self.savedSpecifiers[@"SliderCell-4"] afterSpecifierID:@"GroupCell-8" animated:YES];
+
+        }
+
+    }
 
 }
 
@@ -239,6 +610,15 @@ static NSString *takeMeThere = @"/var/mobile/Library/Preferences/me.luki.amēlij
 
 
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"https://twitter.com/BenOwl3"] options:@{} completionHandler:nil];
+
+
+}
+
+
+- (void)miguelaka {
+
+
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"https://www.reddit.com/user/Miguelaka95/"] options:@{} completionHandler:nil];
 
 
 }
