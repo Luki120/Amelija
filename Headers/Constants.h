4d9b9bbe7f1232039773c43d3dbc87f6e5b6064c
luki120 <luki120@exerra.xyz>
2022-06-12 01:08:40 -0300

    .

--- Headers/Constants.h
+++ Headers/Constants.h
@@ -5,4 +5,3 @@ static NSString *const kPath = @"/var/mobile/Library/Preferences/me.luki.amēlij
 
 #define kAmelijaTintColor [UIColor colorWithRed: 0.47 green: 0.21 blue: 0.24 alpha: 1.0]
 #define kTakoExists [[NSFileManager defaultManager] fileExistsAtPath:@"/Library/MobileSubstrate/DynamicLibraries/Tako.dylib"]
-
