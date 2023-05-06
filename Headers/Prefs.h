93757232bfd664c3554516a9e13402df984fcaba
luki120 <luki120@exerra.xyz>
2023-05-06 00:20:39 -0300

    changed default blur value to 0.85

--- Headers/Prefs.h
+++ Headers/Prefs.h
@@ -5,7 +5,7 @@ static BOOL blurIfNotifs;
 
 static NSInteger lsBlurType;
 
-static float lsIntensity = 1.0f;
+static float lsIntensity;
 
 static UIBlurEffect *blurEffect;
 
@@ -15,7 +15,7 @@ static BOOL hsBlur;
 
 static NSInteger hsBlurType;
 
-static float hsIntensity = 1.0f;
+static float hsIntensity;
 
 static void loadPrefs() {
 
@@ -25,10 +25,10 @@ static void loadPrefs() {
 	lsBlur = prefs[@"lsBlur"] ? [prefs[@"lsBlur"] boolValue] : NO;
 	blurIfNotifs = prefs[@"blurIfNotifs"] ? [prefs[@"blurIfNotifs"] boolValue] : NO;
 	lsBlurType = prefs[@"lsBlurType"] ? [prefs[@"lsBlurType"] integerValue] : 0;
-	lsIntensity = prefs[@"lsIntensity"] ? [prefs[@"lsIntensity"] floatValue] : 1.0f;
+	lsIntensity = prefs[@"lsIntensity"] ? [prefs[@"lsIntensity"] floatValue] : 0.85f;
 
 	hsBlur = prefs[@"hsBlur"] ? [prefs[@"hsBlur"] boolValue] : NO;
 	hsBlurType = prefs[@"hsBlurType"] ? [prefs[@"hsBlurType"] integerValue] : 0;
-	hsIntensity = prefs[@"hsIntensity"] ? [prefs[@"hsIntensity"] floatValue] : 1.0f;
+	hsIntensity = prefs[@"hsIntensity"] ? [prefs[@"hsIntensity"] floatValue] : 0.85f;
 
 }
